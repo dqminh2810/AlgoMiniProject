0282616d8e60af4c04040cb3f2a1f11cf32d4ad9
dqminh2810 <dqminh2810@gmail.com>
2020-05-09 21:26:04 +0200

    Generate graph randomly

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import FordFukerson.FlowNetwork;
 import MinCut.Graph;
+import Utils.RandomConnectedGraph;
 
 import java.io.FileNotFoundException;
 
@@ -7,20 +8,31 @@ public class Main {
 
     public static void main(String[] args) throws FileNotFoundException, CloneNotSupportedException {
 
+        //Generate random graph with given nb of edges
+        RandomConnectedGraph r= new RandomConnectedGraph(25);
+        r.buildRandomGraph();
+        r.printAdjacents();
+        r.exportTxtFile();
 
+        //Test with min cut algo
         ClassLoader loader = Main.class.getClassLoader();
-        Graph g = new Graph("resources/SimpleInput.txt");
+        Graph g = new Graph("resources/randomGraph.txt");
         g.printGraph();
         //System.out.println(g.calculateMaxFlowValue(g.vertices.get(3), g.vertices.get(10)));
         //System.out.println(g.calculateMaxFlowValue());
         System.out.println(g.findMinCut());
 
 
-        FlowNetwork f = new FlowNetwork("resources/SimpleInput.txt");
+        //Test with ford fukerson algo
+        FlowNetwork f = new FlowNetwork("resources/randomGraph.txt");
         //f.printGraph();
         //System.out.println(g.calculateMaxFlowValue(g.vertices.get(3), g.vertices.get(10)));
         //f.setSrcSink(10, 7);
         System.out.println(f.calculateMaxFlowValue());
+
+
+
+
 
     }
 }
---
+++ src/Utils/RandomConnectedGraph.java
@@ -0,0 +1,137 @@
+package Utils;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
+public class RandomConnectedGraph {
+
+    private ArrayList<Integer> vertices;
+    public HashMap<Integer, ArrayList<Integer>> adjacencyList;
+    public int nbVertices, nbEdges;
+
+    public RandomConnectedGraph(int nbEdges)
+    {
+        this.nbEdges = nbEdges;
+        int minV = (int) Math.ceil((1 + Math.sqrt(1 + 8 * nbEdges)) / 2);
+        int maxV = nbEdges + 1;
+
+        Random random = new Random();
+        nbVertices = Math.abs(random.nextInt(maxV - minV) + minV);
+
+        vertices = new ArrayList<>();
+        adjacencyList = new HashMap<Integer, ArrayList<Integer>>();
+        for (int i = 1; i <= nbVertices; i++)
+            adjacencyList.put(i, new ArrayList<>());
+    }
+
+    public void setEdge(int to, int from)
+    {
+        if (to > adjacencyList.size() || from > adjacencyList.size())
+            System.out.println("The vertices does not exists");
+        adjacencyList.get(to).add(from);
+        adjacencyList.get(from).add(to);
+
+        /*
+        ArrayList<Integer> sls = adjacencyList.get(to);
+        sls.add(from);
+        ArrayList<Integer> dls = adjacencyList.get(from);
+        dls.add(to);
+        */
+    }
+
+    public ArrayList<Integer> getEdge(int to)
+    {
+        if (to > adjacencyList.size())
+        {
+            System.out.println("The vertices does not exists");
+            return null;
+        }
+        return adjacencyList.get(to);
+    }
+
+
+    public void buildRandomGraph(){
+        try
+        {
+            int count = 1, to, from;
+            while (count <= nbEdges)
+            {
+                Random random = new Random();
+
+                from = Math.abs(random.nextInt(nbVertices + 1 - 1) + 1);
+                to = Math.abs(random.nextInt(nbVertices + 1 - 1) + 1);
+
+                if(to != from){
+                    if(!adjacencyList.get(to).contains(from) && !adjacencyList.get(from).contains(to)){
+                        setEdge(to, from);
+                        //reg.setEdge(from, to);
+                        if(!vertices.contains(to)) vertices.add(to);
+                        if(!vertices.contains(from)) vertices.add(from);
+                        count++;
+                    }
+                }
+            }
+
+            adjacencyList.forEach((k,v) -> {
+               if(v.size() == 0){
+                   Random random = new Random();
+                   int to2 = k;
+                   while(to2 == k){
+                        to2 = Math.abs(random.nextInt(nbVertices + 1 - 1) + 1);
+                        if(to2!=k) {
+                            setEdge(to2, k);
+                            nbEdges++;
+                        }
+
+                   }
+
+               }
+            });
+        }
+        catch (Exception E)
+        {
+            System.out.println("Something went wrong");
+        }
+    }
+
+    public void printAdjacents(){
+        System.out.println("Random graph has "+nbVertices+" vertices and " +nbEdges+ " edges");
+
+        adjacencyList.forEach((k,v) -> {
+            System.out.print(k+":  ");
+            for(int vertexId: v){
+                System.out.print(vertexId +"\t");
+            }
+
+            System.out.println();
+        });
+    }
+
+    public HashMap<Integer, ArrayList<Integer>> getAdjacencyList(){
+        return adjacencyList;
+    }
+
+    public void exportTxtFile(){
+        try {
+            FileWriter myWriter = new FileWriter(new File("resources", "randomGraph.txt"));
+            adjacencyList.forEach((k,v) -> {
+                try {
+                    myWriter.write(k+"\t");
+                    for(int adjacent: v){
+                        myWriter.write(adjacent+"\t");
+                    }
+                    myWriter.write("\n");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            myWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
